878dbf0ca68cd790e417a164a23ddd69ebce93a6
Yisrael Meir Sobel <yisraelmeirsobel@gmail.com>
2026-06-23 07:44:26 +0300

    Add borrow_book function

--- library.py
+++ library.py
@@ -4,7 +4,22 @@ books = {}
 readers = {}
 loans = {}
 # ========== LOAN FUNCTIONS ==========
-# TODO: borrow_book(book_id, reader_id)
+def borrow_book(book_id, reader_id):
+    if book_id not in books:
+        print("Book not found")
+        return False
+    if not books[book_id]["available"]:
+        print("Book not available")
+        return False
+    books[book_id]["available"] = False
+    readers[reader_id]["books_borrowed"].append(book_id)
+    due_date = datetime.now() + timedelta(days=14)
+    loans[book_id] = {
+        "reader_id": reader_id,
+        "due_date": due_date.strftime("%Y-%m-%d")
+    }
+    print(f"Book borrowed successfully. Due date: {loans[book_id]['due_date']}")
+    return True
 # TODO: return_book(book_id)
 # TODO: extend_loan(book_id, days)
 # ========== REPORT FUNCTIONS ==========
